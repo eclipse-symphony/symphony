411adb440bd8997df4ea2b6f3f01642312dc1f1a
Sailee Rane <saileerane@microsoft.com>
2023-06-29 20:59:07 +0000

    Merged PR 8362979: Updating the Http & Script Provider Test Coverage
    - Adding new unit tests to HTTP and script provider

--- api/pkg/apis/v1alpha1/providers/target/http/http_test.go
+++ api/pkg/apis/v1alpha1/providers/target/http/http_test.go
@@ -2,28 +2,45 @@ package http
 
 import (
 	"context"
-	"os"
 	"testing"
 
 	"github.com/azure/symphony/api/pkg/apis/v1alpha1/model"
 	"github.com/azure/symphony/api/pkg/apis/v1alpha1/providers/target/conformance"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+// TestHttpTargetProviderConfigFromMapNil tests that HttpTargetProviderConfigFromMap returns nil when passed nil
 func TestHttpTargetProviderConfigFromMapNil(t *testing.T) {
 	_, err := HttpTargetProviderConfigFromMap(nil)
-	assert.Nil(t, err)
-}
-func TestHttpTargetProviderConfigFromMapEmpty(t *testing.T) {
-	_, err := HttpTargetProviderConfigFromMap(map[string]string{})
-	assert.Nil(t, err)
+	require.Nil(t, err)
 }
 
+// TestHttpTargetProviderConfigFromMapEmpty tests that HttpTargetProviderConfigFromMap returns nil when passed an empty map
+func TestHttpTargetProviderConfigFromMapEmpty(t *testing.T) {
+	_, err := HttpTargetProviderConfigFromMap(map[string]string{})
+	require.Nil(t, err)
+}
+
+// TestHttpTargetProviderConfigFromMap tests that HttpTargetProviderConfigFromMap returns nil when passed a valid map
+func TestHttpTargetProviderConfigFromMap(t *testing.T) {
+	_, err := HttpTargetProviderConfigFromMap(map[string]string{
+		"name": "test",
+	})
+	require.Nil(t, err)
+}
+
+// TestHttpTargetProviderInitWithMap tests that HttpTargetProvider.InitWithMap returns nil when passed a non empty map
+func TestHttpTargetProviderInitWithMap(t *testing.T) {
+	provider := HttpTargetProvider{}
+	err := provider.InitWithMap(map[string]string{
+		"name": "test",
+	})
+	require.Nil(t, err)
+}
+
+// TestHttpTargetproviderApply tests that HttpTargetProvider.Apply returns nil when passed a valid deployment spec
 func TestHttpTargetProviderApply(t *testing.T) {
-	testLogicApps := os.Getenv("TEST_LOGIC_APPS")
-	if testLogicApps == "" {
-		t.Skip("Skipping because TEST_LOGIC_APPS enviornment variable is not set")
-	}
 	config := HttpTargetProviderConfig{
 		Name: "qa-target",
 	}
@@ -36,9 +53,8 @@ func TestHttpTargetProviderApply(t *testing.T) {
 				{
 					Name: "http-component",
 					Properties: map[string]interface{}{
-						"http.url":    "https://manual-approval.azurewebsites.net:443/api/approval/triggers/manual/invoke?api-version=2022-05-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=<redacted>",
-						"http.method": "POST",
-						"http.body":   `{"solution":"$solution()", "instance": "$instance()", "target": "$target()", "id": "$instance()-$solution()-$target()"}`,
+						"http.url":    "https://learn.microsoft.com/en-us/content-nav/azure.json?",
+						"http.method": "GET",
 					},
 				},
 			},
@@ -64,6 +80,168 @@ func TestHttpTargetProviderApply(t *testing.T) {
 	}, false)
 	assert.Nil(t, err)
 }
+
+// TestHttpTargetProviderIncorrectApply tests that HttpTargetProvider.Apply returns an error when passed an invalid deployment spec
+func TestHttpTargetProviderIncorrectApply(t *testing.T) {
+	config := HttpTargetProviderConfig{
+		Name: "qa-target",
+	}
+	provider := HttpTargetProvider{}
+	err := provider.Init(config)
+	assert.Nil(t, err)
+	err = provider.Apply(context.Background(), model.DeploymentSpec{
+		Solution: model.SolutionSpec{
+			Components: []model.ComponentSpec{
+				{
+					Name: "http-component",
+					Properties: map[string]interface{}{
+						"http.url":    "",
+						"http.method": "GET",
+					},
+				},
+			},
+		},
+	}, false)
+	assert.NotNil(t, err)
+}
+
+// TestHttpTargetProviderGet tests that HttpTargetProvider.Get returns nil when passed a valid deployment spec
+func TestHttpTargetProviderGet(t *testing.T) {
+	config := HttpTargetProviderConfig{
+		Name: "qa-target",
+	}
+	provider := HttpTargetProvider{}
+	err := provider.Init(config)
+	assert.Nil(t, err)
+	_, err = provider.Get(context.Background(), model.DeploymentSpec{
+		Solution: model.SolutionSpec{
+			Components: []model.ComponentSpec{
+				{
+					Name: "http-component",
+					Properties: map[string]interface{}{
+						"http.url":    "https://learn.microsoft.com/en-us/content-nav/azure.json?",
+						"http.method": "GET",
+					},
+				},
+			},
+		},
+		Assignments: map[string]string{
+			"target-1": "{http-component}",
+		},
+		Targets: map[string]model.TargetSpec{
+			"target-1": {
+				Topologies: []model.TopologySpec{
+					{
+						Bindings: []model.BindingSpec{
+							{
+								Role:     "instance",
+								Provider: "doesn't-matter",
+								Config:   map[string]string{},
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+	assert.Nil(t, err)
+}
+
+// TestHttpTargetProviderRemove tests that HttpTargetProvider.Remove returns nil when passed a valid deployment spec
+func TestHttpTargetProviderRemove(t *testing.T) {
+	config := HttpTargetProviderConfig{
+		Name: "qa-target",
+	}
+	provider := HttpTargetProvider{}
+	err := provider.Init(config)
+	assert.Nil(t, err)
+	err = provider.Remove(context.Background(), model.DeploymentSpec{
+		Solution: model.SolutionSpec{
+			Components: []model.ComponentSpec{
+				{
+					Name: "http-component",
+					Properties: map[string]interface{}{
+						"http.url":    "https://learn.microsoft.com/en-us/content-nav/azure.json?",
+						"http.method": "GET",
+					},
+				},
+			},
+		},
+		Assignments: map[string]string{
+			"target-1": "{http-component}",
+		},
+		Targets: map[string]model.TargetSpec{
+			"target-1": {
+				Topologies: []model.TopologySpec{
+					{
+						Bindings: []model.BindingSpec{
+							{
+								Role:     "instance",
+								Provider: "doesn't-matter",
+								Config:   map[string]string{},
+							},
+						},
+					},
+				},
+			},
+		},
+	}, []model.ComponentSpec{})
+	assert.Nil(t, err)
+}
+
+// TestHttpTargetProviderNeedsUpdate tests that HttpTargetProvider.NeedsUpdate returns false when passed a valid deployment spec
+func TestHttpTargetProviderNeedsUpdate(t *testing.T) {
+	current := []model.ComponentSpec{
+		{
+			Name: "http-component",
+			Properties: map[string]interface{}{
+				"http.url":    "https://learn.microsoft.com/en-us/content-nav/azure.json?",
+				"http.method": "GET",
+			},
+		},
+	}
+
+	desired := []model.ComponentSpec{
+		{
+			Name: "http-component",
+			Properties: map[string]interface{}{
+				"http.url":    "https://learn.microsoft.com/en-us/content-nav/azure.json?",
+				"http.method": "GET",
+			},
+		},
+	}
+	provider := HttpTargetProvider{}
+	update := provider.NeedsUpdate(context.Background(), current, desired)
+	require.False(t, update)
+}
+
+// TestHttpTargetProviderNeedsRemove tests that HttpTargetProvider.NeedsRemove returns false when passed a valid deployment spec
+func TestHttpTargetProviderNeedsRemove(t *testing.T) {
+	current := []model.ComponentSpec{
+		{
+			Name: "http-component",
+			Properties: map[string]interface{}{
+				"http.url":    "https://learn.microsoft.com/en-us/content-nav/azure.json?",
+				"http.method": "GET",
+			},
+		},
+	}
+
+	desired := []model.ComponentSpec{
+		{
+			Name: "http-component",
+			Properties: map[string]interface{}{
+				"http.url":    "https://learn.microsoft.com/en-us/content-nav/azure.json?",
+				"http.method": "GET",
+			},
+		},
+	}
+	provider := HttpTargetProvider{}
+	remove := provider.NeedsRemove(context.Background(), current, desired)
+	require.True(t, remove)
+}
+
+// TestReadProperty tests that ReadProperty returns the correct value
 func TestReadProperty(t *testing.T) {
 	url := "https://manual-approval.azurewebsites.net:443/api/approval/triggers/manual/invoke?api-version=2022-05-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=<redacted>"
 	val := model.ReadProperty(map[string]string{
@@ -76,6 +254,7 @@ func TestReadProperty(t *testing.T) {
 	assert.Equal(t, url, val)
 }
 
+// TestConformanceSuite tests that the HttpTargetProvider conforms to the TargetProvider interface
 func TestConformanceSuite(t *testing.T) {
 	provider := &HttpTargetProvider{}
 	err := provider.Init(HttpTargetProviderConfig{})
--- api/pkg/apis/v1alpha1/providers/target/script/script_test.go
+++ api/pkg/apis/v1alpha1/providers/target/script/script_test.go
@@ -8,9 +8,10 @@ import (
 
 	"github.com/azure/symphony/api/pkg/apis/v1alpha1/model"
 	"github.com/azure/symphony/api/pkg/apis/v1alpha1/providers/target/conformance"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+// TestInitMissingGet tests that we can init with a map fails if get is missing
 func TestInitMissingGet(t *testing.T) {
 	provider := ScriptProvider{}
 	err := provider.InitWithMap(map[string]string{
@@ -19,9 +20,10 @@ func TestInitMissingGet(t *testing.T) {
 		"applyScript":   "a",
 		"removeScript":  "b",
 	})
-	assert.NotNil(t, err)
+	require.NotNil(t, err)
 }
 
+// TestInitMissingApply tests that we can init with a map fails if apply is missing
 func TestInitMissingApply(t *testing.T) {
 	provider := ScriptProvider{}
 	err := provider.InitWithMap(map[string]string{
@@ -30,8 +32,10 @@ func TestInitMissingApply(t *testing.T) {
 		"getScript":     "a",
 		"removeScript":  "b",
 	})
-	assert.NotNil(t, err)
+	require.NotNil(t, err)
 }
+
+// TestInitMissingRemove tests that we can init with a map fails if remove is missing
 func TestInitMissingRemove(t *testing.T) {
 	provider := ScriptProvider{}
 	err := provider.InitWithMap(map[string]string{
@@ -40,8 +44,27 @@ func TestInitMissingRemove(t *testing.T) {
 		"getScript":     "a",
 		"applyScript":   "b",
 	})
-	assert.NotNil(t, err)
+	require.NotNil(t, err)
 }
+
+// TestInitWithMap tests that we can init with a map
+func TestInitWithMap(t *testing.T) {
+	provider := ScriptProvider{}
+	err := provider.InitWithMap(map[string]string{
+		"name":          "test",
+		"needsUpdate":   "mock-needsupdate.sh",
+		"needsRemove":   "mock-needsremove.sh",
+		"stagingFolder": ".",
+		"scriptFolder":  "https://demopolicies.blob.core.windows.net/gatekeeper",
+		"applyScript":   "mock-apply.sh",
+		"removeScript":  "mock-remove.sh",
+		"getScript":     "mock-get.sh",
+		"scriptEngine":  "bash",
+	})
+	require.Nil(t, err)
+}
+
+// TestGet tests that we can get a script
 func TestGet(t *testing.T) {
 	provider := ScriptProvider{}
 	currentFolder, _ := filepath.Abs(".")
@@ -49,7 +72,7 @@ func TestGet(t *testing.T) {
 		ScriptFolder: "",
 		GetScript:    filepath.Join(currentFolder, "mock-get.sh"),
 	})
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	components, err := provider.Get(context.Background(), model.DeploymentSpec{
 		Solution: model.SolutionSpec{
 			Components: []model.ComponentSpec{
@@ -60,13 +83,15 @@ func TestGet(t *testing.T) {
 		},
 	})
 
-	assert.Nil(t, err)
-	assert.Equal(t, 1, len(components))
+	require.Nil(t, err)
+	require.Equal(t, 1, len(components))
 }
+
+// TestNeedsUpdateEmptyScript tests that we can update a script
 func TestNeedsUpdateEmptyScript(t *testing.T) {
 	provider := ScriptProvider{}
 	err := provider.Init(ScriptProviderConfig{})
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	b := provider.NeedsUpdate(context.Background(), []model.ComponentSpec{
 		{
 			Name: "com1",
@@ -78,15 +103,17 @@ func TestNeedsUpdateEmptyScript(t *testing.T) {
 			},
 		})
 
-	assert.Nil(t, err)
-	assert.False(t, b)
+	require.Nil(t, err)
+	require.False(t, b)
 }
+
+// TestNeedsUpdateScript tests that we can update a script
 func TestNeedsUpdateScript(t *testing.T) {
 	provider := ScriptProvider{}
 	err := provider.Init(ScriptProviderConfig{
 		NeedsUpdate: "mock-needsupdate.sh",
 	})
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	b := provider.NeedsUpdate(context.Background(), []model.ComponentSpec{
 		{
 			Name: "com1",
@@ -98,13 +125,15 @@ func TestNeedsUpdateScript(t *testing.T) {
 			},
 		})
 
-	assert.Nil(t, err)
-	assert.True(t, b)
+	require.Nil(t, err)
+	require.True(t, b)
 }
+
+// TestNeedsRemoveEmptyScript tests that we can remove a script
 func TestNeedsRemoveEmptyScript(t *testing.T) {
 	provider := ScriptProvider{}
 	err := provider.Init(ScriptProviderConfig{})
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	b := provider.NeedsRemove(context.Background(), []model.ComponentSpec{
 		{
 			Name: "com1",
@@ -116,15 +145,17 @@ func TestNeedsRemoveEmptyScript(t *testing.T) {
 			},
 		})
 
-	assert.Nil(t, err)
-	assert.True(t, b)
+	require.Nil(t, err)
+	require.True(t, b)
 }
+
+// TestNeedsRemoveScript tests that we can remove a script
 func TestNeedsRemoveScript(t *testing.T) {
 	provider := ScriptProvider{}
 	err := provider.Init(ScriptProviderConfig{
 		NeedsRemove: "mock-needsremove.sh",
 	})
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	b := provider.NeedsRemove(context.Background(), []model.ComponentSpec{
 		{
 			Name: "com1",
@@ -136,15 +167,17 @@ func TestNeedsRemoveScript(t *testing.T) {
 			},
 		})
 
-	assert.Nil(t, err)
-	assert.False(t, b)
+	require.Nil(t, err)
+	require.False(t, b)
 }
+
+// TestRemoveScript tests that we can remove a script
 func TestRemoveScript(t *testing.T) {
 	provider := ScriptProvider{}
 	err := provider.Init(ScriptProviderConfig{
 		RemoveScript: "mock-remove.sh",
 	})
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	err = provider.Remove(context.Background(), model.DeploymentSpec{},
 		[]model.ComponentSpec{
 			{
@@ -152,19 +185,21 @@ func TestRemoveScript(t *testing.T) {
 			},
 		})
 
-	assert.Nil(t, err)
+	require.Nil(t, err)
 }
 
+// TestApplyScript tests that we can apply a script
 func TestApplyScript(t *testing.T) {
 	provider := ScriptProvider{}
 	err := provider.Init(ScriptProviderConfig{
 		ApplyScript: "mock-apply.sh",
 	})
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	err = provider.Apply(context.Background(), model.DeploymentSpec{}, false)
-	assert.Nil(t, err)
+	require.Nil(t, err)
 }
 
+// TestGetScriptFromUrl tests that we can get a script from a url
 func TestGetScriptFromUrl(t *testing.T) {
 	testScriptProvider := os.Getenv("TEST_SCRIPT_PROVIDER")
 	if testScriptProvider == "" {
@@ -177,13 +212,13 @@ func TestGetScriptFromUrl(t *testing.T) {
 		RemoveScript: "mock-remove.sh",
 		ScriptFolder: "https://demopolicies.blob.core.windows.net/gatekeeper",
 	})
-	assert.Nil(t, err)
+	require.Nil(t, err)
 }
 
 // Conformance: you should call the conformance suite to ensure provider conformance
 func TestConformanceSuite(t *testing.T) {
 	provider := &ScriptProvider{}
 	err := provider.Init(ScriptProviderConfig{})
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	conformance.ConformanceSuite(t, provider)
 }
