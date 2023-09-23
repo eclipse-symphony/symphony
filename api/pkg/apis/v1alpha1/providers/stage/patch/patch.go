dcefb17112959d0e73f435dae8139945b7fe772a
Haishi Bai <hbai@microsoft.com>
2023-09-22 18:43:20 -0700

    patcher

---
+++ api/pkg/apis/v1alpha1/providers/stage/patch/patch.go
@@ -0,0 +1,298 @@
+/*
+
+	MIT License
+
+	Copyright (c) Microsoft Corporation.
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE
+
+*/
+
+package patch
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"sync"
+
+	"github.com/azure/symphony/api/pkg/apis/v1alpha1/providers/stage"
+	"github.com/azure/symphony/api/pkg/apis/v1alpha1/utils"
+	"github.com/azure/symphony/coa/pkg/apis/v1alpha2"
+	"github.com/azure/symphony/coa/pkg/apis/v1alpha2/contexts"
+	"github.com/azure/symphony/coa/pkg/apis/v1alpha2/providers"
+	"github.com/azure/symphony/coa/pkg/logger"
+)
+
+var msLock sync.Mutex
+var sLog = logger.NewLogger("coa.runtime")
+
+type PatchStageProviderConfig struct {
+	BaseUrl  string `json:"baseUrl"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+}
+
+type PatchStageProvider struct {
+	Config  PatchStageProviderConfig
+	Context *contexts.ManagerContext
+}
+
+func (s *PatchStageProvider) Init(config providers.IProviderConfig) error {
+	msLock.Lock()
+	defer msLock.Unlock()
+	mockConfig, err := toPatchStageProviderConfig(config)
+	if err != nil {
+		return err
+	}
+	s.Config = mockConfig
+	return nil
+}
+func (s *PatchStageProvider) SetContext(ctx *contexts.ManagerContext) {
+	s.Context = ctx
+}
+func toPatchStageProviderConfig(config providers.IProviderConfig) (PatchStageProviderConfig, error) {
+	ret := PatchStageProviderConfig{}
+	data, err := json.Marshal(config)
+	if err != nil {
+		return ret, err
+	}
+	err = json.Unmarshal(data, &ret)
+	return ret, err
+}
+func (i *PatchStageProvider) InitWithMap(properties map[string]string) error {
+	config, err := SymphonyStageProviderConfigFromMap(properties)
+	if err != nil {
+		return err
+	}
+	return i.Init(config)
+}
+func SymphonyStageProviderConfigFromMap(properties map[string]string) (PatchStageProviderConfig, error) {
+	ret := PatchStageProviderConfig{}
+	baseUrl, err := utils.GetString(properties, "baseUrl")
+	if err != nil {
+		return ret, err
+	}
+	ret.BaseUrl = baseUrl
+	if ret.BaseUrl == "" {
+		return ret, v1alpha2.NewCOAError(nil, "baseUrl is required", v1alpha2.BadConfig)
+	}
+	user, err := utils.GetString(properties, "user")
+	if err != nil {
+		return ret, err
+	}
+	ret.User = user
+	if ret.User == "" {
+		return ret, v1alpha2.NewCOAError(nil, "user is required", v1alpha2.BadConfig)
+	}
+	password, err := utils.GetString(properties, "password")
+	if err != nil {
+		return ret, err
+	}
+	ret.Password = password
+	return ret, nil
+}
+func (m *PatchStageProvider) traceValue(v interface{}) (interface{}, error) {
+	switch val := v.(type) {
+	case string:
+		parser := utils.NewParser(val)
+		context := m.Context.VencorContext.EvaluationContext.Clone()
+		v, err := parser.Eval(*context)
+		if err != nil {
+			return "", err
+		}
+		switch vt := v.(type) {
+		case string:
+			return vt, nil
+		default:
+			return m.traceValue(v)
+		}
+	case int:
+		return strconv.Itoa(val), nil
+	case int32:
+		return strconv.Itoa(int(val)), nil
+	case int64:
+		return strconv.Itoa(int(val)), nil
+	case float32:
+		return strconv.FormatFloat(float64(val), 'f', -1, 32), nil
+	case float64:
+		return strconv.FormatFloat(val, 'f', -1, 64), nil
+	case bool:
+		return strconv.FormatBool(val), nil
+	case []interface{}:
+		ret := []interface{}{}
+		for _, v := range val {
+			tv, err := m.traceValue(v)
+			if err != nil {
+				return "", err
+			}
+			ret = append(ret, tv)
+		}
+		return ret, nil
+	case map[string]interface{}:
+		ret := map[string]interface{}{}
+		for k, v := range val {
+			tv, err := m.traceValue(v)
+			if err != nil {
+				return "", err
+			}
+			ret[k] = tv
+		}
+		return ret, nil
+	default:
+		return fmt.Sprintf("%v", v), nil
+	}
+}
+
+func (i *PatchStageProvider) Process(ctx context.Context, inputs map[string]interface{}) (map[string]interface{}, bool, error) {
+	sLog.Info("  P (Patch Stage): start process request")
+
+	outputs := make(map[string]interface{})
+
+	objectType := stage.ReadInputString(inputs, "objectType")
+	objectName := stage.ReadInputString(inputs, "objectName")
+	patchSource := stage.ReadInputString(inputs, "patchSource")
+	componentName := stage.ReadInputString(inputs, "component")
+	propertyName := stage.ReadInputString(inputs, "property")
+	subKey := stage.ReadInputString(inputs, "subKey")
+	dedupKey := stage.ReadInputString(inputs, "dedupKey")
+	patchAction := stage.ReadInputString(inputs, "patchAction")
+	if patchAction == "" {
+		patchAction = "add"
+	}
+	udpated := false
+
+	switch objectType {
+	case "solution":
+		solution, err := utils.GetSolution(i.Config.BaseUrl, objectName, i.Config.User, i.Config.Password)
+		if err != nil {
+			sLog.Errorf("  P (Patch Stage): error getting solution %s", objectName)
+			return nil, false, err
+		}
+		catalog, err := utils.GetCatalog(i.Config.BaseUrl, patchSource, i.Config.User, i.Config.Password)
+
+		if err != nil {
+			sLog.Errorf("  P (Patch Stage): error getting catalog %s", patchSource)
+			return nil, false, err
+		}
+
+		for k, v := range catalog.Spec.Properties {
+			tv, err := i.traceValue(v)
+			if err != nil {
+				sLog.Errorf("  P (Patch Stage): error tracing value %s", k)
+				return nil, false, err
+			}
+			catalog.Spec.Properties[k] = tv
+		}
+
+		for i, c := range solution.Spec.Components {
+			if c.Name == componentName {
+				for k, p := range c.Properties {
+					if k == propertyName {
+						if subKey != "" {
+							if detailedTarget, ok := p.(map[string]interface{}); ok {
+								if v, ok := detailedTarget[subKey]; ok {
+									if targetMap, ok := v.([]interface{}); ok {
+										replaced := false
+										if dedupKey != "" {
+											for i, v := range targetMap {
+												if vmap, ok := v.(map[string]interface{}); ok {
+													if vmap[dedupKey] == catalog.Spec.Properties[dedupKey] {
+														if patchAction == "remove" {
+															targetMap = append(targetMap[:i], targetMap[i+1:]...)
+														} else {
+															targetMap[i] = catalog.Spec.Properties
+														}
+														replaced = true
+														break
+													}
+												}
+											}
+										}
+										if !replaced && patchAction != "remove" {
+											targetMap = append(targetMap, catalog.Spec.Properties)
+										}
+										detailedTarget[subKey] = targetMap
+										solution.Spec.Components[i].Properties[propertyName] = detailedTarget
+										udpated = true
+									} else {
+										sLog.Errorf("  P (Patch Stage): target properties is not valid")
+										return nil, false, v1alpha2.NewCOAError(nil, "target properties is not valid", v1alpha2.BadConfig)
+									}
+								} else {
+									sLog.Errorf("  P (Patch Stage): subKey is not valid")
+									return nil, false, v1alpha2.NewCOAError(nil, "subKey is not valid", v1alpha2.BadConfig)
+								}
+							} else {
+								sLog.Errorf("  P (Patch Stage): subKey is not valid")
+								return nil, false, v1alpha2.NewCOAError(nil, "subKey is not valid", v1alpha2.BadConfig)
+							}
+						} else {
+							if targetMap, ok := p.([]interface{}); ok {
+								replaced := false
+								if dedupKey != "" {
+									for i, v := range targetMap {
+										if vmap, ok := v.(map[string]interface{}); ok {
+											if vmap[dedupKey] == catalog.Spec.Properties[dedupKey] {
+												if patchAction == "remove" {
+													targetMap = append(targetMap[:i], targetMap[i+1:]...)
+												} else {
+													targetMap[i] = catalog.Spec.Properties
+												}
+												replaced = true
+												break
+											}
+										}
+									}
+								}
+								if !replaced && patchAction != "remove" {
+									targetMap = append(targetMap, catalog.Spec.Properties)
+								}
+								solution.Spec.Components[i].Properties[propertyName] = targetMap
+								udpated = true
+							} else {
+								sLog.Errorf("  P (Patch Stage): target properties is not valid")
+								return nil, false, v1alpha2.NewCOAError(nil, "target properties is not valid", v1alpha2.BadConfig)
+							}
+						}
+						break
+					}
+				}
+				break
+			}
+		}
+		if udpated {
+			jData, _ := json.Marshal(solution.Spec)
+			err := utils.UpsertSolution(i.Config.BaseUrl, objectName, i.Config.User, i.Config.Password, jData)
+			if err != nil {
+				sLog.Errorf("  P (Patch Stage): error updating solution %s", objectName)
+				return nil, false, err
+			}
+		}
+
+	}
+	if udpated {
+		outputs["status"] = "OK"
+	} else {
+		outputs["status"] = "Skipped"
+	}
+	sLog.Info("  P (Patch Stage): end process request")
+	return outputs, false, nil
+}
