7328e3d1cdb5cefedcc9fad183f698f9d0d74566
Haishi Bai <hbai@microsoft.com>
2023-11-17 22:26:11 -0800

    wasm, ebfp, flatcar

---
+++ docs/samples/piccolo/hello-ebpf/hello.bpf.c
@@ -0,0 +1,13 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+int counter = 0;
+
+SEC("xdp")
+int hello(struct xdp_md *ctx) {
+    bpf_printk("Hello World %d", counter);
+    counter++;
+    return XDP_PASS;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
