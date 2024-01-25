d6d1469a1a6dfab411c99cb446de04aa996f9d0d
Haishi2016 <hbai@microsoft.com>
2024-01-25 08:57:50 -0800

    Add approval workflow samples (#82)
    * approval flow fixes
    
    * script approval
    
    * update readme root

--- api/pkg/apis/v1alpha1/providers/target/configmap/configmap.go
+++ api/pkg/apis/v1alpha1/providers/target/configmap/configmap.go
@@ -317,6 +317,10 @@ func (k *ConfigMapTargetProvider) ensureNamespace(ctx context.Context, namespace
 	var err error = nil
 	defer utils.CloseSpanWithError(span, &err)
 
+	if namespace == "" || namespace == "default" {
+		return nil
+	}
+
 	_, err = k.Client.CoreV1().Namespaces().Get(ctx, namespace, metav1.GetOptions{})
 	if err == nil {
 		return nil
