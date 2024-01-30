7fed61529d2e3ca6fed677ed906f607f34c57128
Lency Qian <36918585+LeiQL@users.noreply.github.com>
2024-01-31 06:59:56 +0800

    Guard invalid null request ID in http and k8s state providers (#87)
    * guard request ID null in state providers
    
    * handle badrequest

--- coa/pkg/apis/v1alpha2/providers/states/httpstate/httpstate_test.go
+++ coa/pkg/apis/v1alpha2/providers/states/httpstate/httpstate_test.go
@@ -9,6 +9,7 @@ package httpstate
 import (
 	"context"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -237,12 +238,48 @@ func TestGet(t *testing.T) {
 func TestUpsertGetDelete(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
+			relativePath := r.URL.Path
+			if relativePath == "" || relativePath == "/" {
+				http.Error(w, "invalid path", http.StatusBadRequest)
+				return
+			}
 			response := map[string]interface{}{
 				"key": "abc",
 			}
 			jsonResponse, _ := json.Marshal(response)
 			w.Header().Set("Content-Type", "application/json")
 			w.Write(jsonResponse)
+		} else if r.Method == "POST" {
+			var data []map[string]interface{}
+			body, _ := ioutil.ReadAll(r.Body)
+			err := json.Unmarshal([]byte(body), &data)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			if len(data) == 0 {
+				http.Error(w, "invalid request body", http.StatusBadRequest)
+				return
+			}
+			firstItemKey := data[0]["key"].(string)
+			if firstItemKey == "" {
+				http.Error(w, "invalid request body", http.StatusBadRequest)
+				return
+			}
+			response := map[string]interface{}{
+				"key":   data[0]["key"],
+				"value": data[0]["value"],
+			}
+			jsonResponse, _ := json.Marshal(response)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonResponse)
+		} else if r.Method == "DELETE" {
+			relativePath := r.URL.Path
+			if relativePath == "" || relativePath == "/" {
+				http.Error(w, "invalid path", http.StatusBadRequest)
+				return
+			}
+			w.Write([]byte("OK"))
 		} else {
 			w.Write([]byte("OK"))
 		}
@@ -258,6 +295,7 @@ func TestUpsertGetDelete(t *testing.T) {
 		PostAsArray:       true,
 		NotFoundAs204:     true,
 	})
+	assert.Nil(t, err)
 
 	_, err = provider.Upsert(context.Background(), states.UpsertRequest{
 		Value: states.StateEntry{
@@ -277,6 +315,24 @@ func TestUpsertGetDelete(t *testing.T) {
 		ID: "123",
 	})
 	assert.Nil(t, err)
+
+	_, err = provider.Upsert(context.Background(), states.UpsertRequest{
+		Value: states.StateEntry{
+			ID:   "",
+			Body: TestPayload{},
+		},
+	})
+	assert.NotNil(t, err)
+
+	_, err = provider.Get(context.Background(), states.GetRequest{
+		ID: "",
+	})
+	assert.NotNil(t, err)
+
+	err = provider.Delete(context.Background(), states.DeleteRequest{
+		ID: "",
+	})
+	assert.NotNil(t, err)
 }
 
 func TestClone(t *testing.T) {
