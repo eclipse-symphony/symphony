4bb914685d475f57060a3124d5f762ce58f85e5d
Jesse <iwangjintian@gmail.com>
2024-02-02 05:32:25 +0800

    Add some test cases for COA (#115)
    * add support for maestro local build
    
    * Revert "add support for maestro local build"
    
    This reverts commit eda75349b2ea5d779be3e4b9a51adc32c5fc2f69.
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch main
    # Your branch is up to date with 'origin/main'.
    #
    # Changes to be committed:
    #	modified:   .github/workflows/release.yaml
    #	modified:   cli/cmd/up.go
    #	modified:   packages/helm/symphony/Chart.yaml
    #	modified:   packages/helm/symphony/values.yaml
    #
    # Untracked files:
    #	api/-output.
    #	api/pkg/apis/v1alpha1/providers/stage/script/staging/78813dc6-6d6e-46dd-9853-ef876c20f2b7-output.json
    #	api/pkg/apis/v1alpha1/providers/stage/script/staging/8e6dad56-26c8-4e6a-8434-fdffe24e2063-output.json
    #	api/pkg/apis/v1alpha1/providers/stage/script/staging/916f9889-8b98-4899-b6ed-c7e935a053ef-output.json
    #	api/pkg/apis/v1alpha1/providers/stage/script/staging/a58d2f22-3c31-4471-9bf7-0a74d32d5946-output.json
    #	api/pkg/apis/v1alpha1/providers/stage/script/staging/c6e9d0ab-0bd4-4b8a-9483-a2bb67308deb-output.json
    #	go.work
    #
    
    * add some coa tests

---
+++ coa/pkg/apis/v1alpha2/observability/exporters/types_test.go
@@ -0,0 +1,28 @@
+package exporters
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewConsoleExporter(t *testing.T) {
+	writer := &bytes.Buffer{}
+	exporter, err := NewConsoleExporter(writer)
+	assert.NoError(t, err)
+	assert.NotNil(t, exporter)
+
+	// Test writer is nil case
+	exporter, err = NewConsoleExporter(nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, exporter)
+}
+
+func TestNewZipkinExporter(t *testing.T) {
+	url := "http://localhost:9411/api/v2/spans"
+	sampleRate := "0.5"
+	exporter, err := NewZipkinExporter(url, sampleRate)
+	assert.NoError(t, err)
+	assert.NotNil(t, exporter)
+}
