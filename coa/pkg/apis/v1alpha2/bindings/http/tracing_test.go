557b9240554396ab52bc650616cea0bfc5053419
Jiawei Du <59427055+msftcoderdjw@users.noreply.github.com>
2024-02-08 02:34:12 +0800

    Fix middleware.http.telemetry config parser issue and add tests (#129)
    * fix middleware.http.telemetry config parse
    
    * add tests in COA bindings
    
    * add sleep seconds to wait for http server startup
    
    * remove default APP_INSIGHT_KEY, will skip telemetry client initialization when key is missing
    
    * skip initialization and raise error when APP_INSIGHT_KEY is missing
    
    * validate otel console exporter and use thread-safe buffer in otel console exporter

--- coa/pkg/apis/v1alpha2/bindings/http/tracing_test.go
+++ coa/pkg/apis/v1alpha2/bindings/http/tracing_test.go
@@ -7,8 +7,17 @@
 package http
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"testing"
+	"time"
 
+	"github.com/eclipse-symphony/symphony/coa/pkg/apis/v1alpha2"
+	observability "github.com/eclipse-symphony/symphony/coa/pkg/apis/v1alpha2/observability"
+	observ_utils "github.com/eclipse-symphony/symphony/coa/pkg/apis/v1alpha2/observability/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/valyala/fasthttp"
 	"go.opentelemetry.io/otel/trace"
@@ -106,4 +115,104 @@ func TestSpanContextFromW3CStringInvalidParentId(t *testing.T) {
 func TestSpanContextFromW3CStringEmpty(t *testing.T) {
 	_, ok := SpanContextFromW3CString("")
 	assert.False(t, ok)
+}
+
+type SpanAttrValue struct {
+	Type  string
+	Value string
+}
+
+type SpanAttr struct {
+	Key   string
+	Value SpanAttrValue
+}
+
+func TestTracing(t *testing.T) {
+	tracing := Tracing{
+		Observability: observability.Observability{},
+	}
+	config := observability.ObservabilityConfig{}
+	config.Pipelines = []observability.PipelineConfig{
+		{
+			Exporter: observability.ExporterConfig{
+				Type:       "tracing.exporters.console",
+				BackendUrl: "",
+				Sampler: observability.SamplerConfig{
+					SampleRate: "always",
+				},
+			},
+		},
+	}
+	var buf v1alpha2.SafeBuffer
+	tracing.Observability.Buffer = &buf
+	err := tracing.Observability.Init(config)
+	assert.Nil(t, err)
+
+	successPath := "/success"
+	successUrl := "http://localhost:7777" + successPath
+	// lauch a HTTP server and validate console tracing in buffer
+	go func() {
+		requestHandler := func(ctx *fasthttp.RequestCtx) {
+			switch string(ctx.Path()) {
+			case successPath:
+				if string(ctx.Method()) == fasthttp.MethodGet {
+					fmt.Fprintf(ctx, "Hello, World!")
+				}
+			default:
+				ctx.Error("Unsupported path", fasthttp.StatusNotFound)
+			}
+		}
+
+		_ = fasthttp.ListenAndServe(":7777", tracing.Tracing(requestHandler))
+	}()
+
+	// wait for server to start
+	time.Sleep(3 * time.Second)
+
+	ctx, span := observability.StartSpan("HTTP-Test-Client", context.Background(), &map[string]string{
+		"method":      "TestTracing",
+		"http.method": fasthttp.MethodGet,
+		"http.url":    successUrl,
+	})
+
+	// GET request with parent span
+	client := &http.Client{}
+	req, err := http.NewRequestWithContext(ctx, fasthttp.MethodGet, successUrl, nil)
+	observ_utils.PropagateSpanContextToHttpRequestHeader(req)
+	assert.Nil(t, err)
+	resp, err := client.Do(req)
+	assert.Nil(t, err)
+
+	defer resp.Body.Close()
+	assert.Equal(t, 200, resp.StatusCode)
+	respBody, err := ioutil.ReadAll(resp.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello, World!", string(respBody))
+
+	// validate the buffer
+	time.Sleep(3 * time.Second) // wait for the span to be written to the buffer
+	spanContent := buf.String()
+	var mapSpan map[string]interface{}
+	json.Unmarshal([]byte(spanContent), &mapSpan)
+	fmt.Println(spanContent)
+
+	assert.Equal(t, span.SpanContext().TraceID().String(), mapSpan["SpanContext"].(map[string]interface{})["TraceID"])
+	assert.Equal(t, span.SpanContext().TraceID().String(), mapSpan["Parent"].(map[string]interface{})["TraceID"])
+	assert.Equal(t, span.SpanContext().SpanID().String(), mapSpan["Parent"].(map[string]interface{})["SpanID"])
+	assert.Equal(t, successPath, mapSpan["Name"])
+
+	attrs, err := json.Marshal(mapSpan["Attributes"])
+	assert.Nil(t, err)
+	spanAttrs := make([]SpanAttr, 0)
+	json.Unmarshal(attrs, &spanAttrs)
+	attrsMap := make(map[string]SpanAttrValue)
+	for _, attr := range spanAttrs {
+		attrsMap[attr.Key] = attr.Value
+	}
+
+	_, expectedVendor := observ_utils.GetVendor(successPath)
+	assert.Equal(t, successPath, attrsMap["path"].Value)
+	assert.Equal(t, expectedVendor, attrsMap["vendor"].Value)
+	assert.Equal(t, fasthttp.MethodGet, attrsMap["method"].Value)
+	assert.Equal(t, "200", attrsMap["status"].Value)
 }
