14743c7c7bd124f5c53e286039e23535e41561af
Jiawei Du <59427055+msftcoderdjw@users.noreply.github.com>
2024-03-25 05:30:27 +0800

    pin setup-envtest module to v0.0.0-20240320141353-395cfc7486e6 for go 1.20.2 (#184)

--- k8s/magefile.go
+++ k8s/magefile.go
@@ -31,7 +31,7 @@ var (
 
 	envTest = bintool.Must(bintool.NewGo(
 		"sigs.k8s.io/controller-runtime/tools/setup-envtest",
-		"latest",
+		"v0.0.0-20240320141353-395cfc7486e6",
 	))
 
 	kustomize = bintool.Must(bintool.New(
