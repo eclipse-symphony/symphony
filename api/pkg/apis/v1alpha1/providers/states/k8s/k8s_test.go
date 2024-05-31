401851c8de5c491d113e5252728c8e2bc8d8878d
Shawn Sun <859005086@qq.com>
2024-05-31 17:06:39 +0800

    fixtest

--- api/pkg/apis/v1alpha1/providers/states/k8s/k8s_test.go
+++ api/pkg/apis/v1alpha1/providers/states/k8s/k8s_test.go
@@ -598,7 +598,7 @@ func TestCatalogFieldFilter(t *testing.T) {
 }
 
 func TestCatalogStatusFilter(t *testing.T) {
-	testK8s := "true"
+	testK8s := os.Getenv("TEST_K8S_STATE")
 	if testK8s == "" {
 		t.Skip("Skipping because TEST_K8S_STATE enviornment variable is not set")
 	}
