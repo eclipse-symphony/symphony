453e5c5b75f964764692e579b963ec9d9f5fca35
Haishi Bai <hbai@microsoft.com>
2024-07-15 14:06:29 -0700

    remove rust provider that was included by mistake

--- api/pkg/apis/v1alpha1/providers/target/rust/target/release/deps/rust_binding-0e2d7c5bf12a94fd.d
+++
@@ -1,7 +0,0 @@
-/home/hbai/projects/go/src/github.com/eclipse-symphony/symphony/api/pkg/apis/v1alpha1/providers/target/rust/target/release/deps/librust_binding-0e2d7c5bf12a94fd.a: rust_binding/src/lib.rs
-
-/home/hbai/projects/go/src/github.com/eclipse-symphony/symphony/api/pkg/apis/v1alpha1/providers/target/rust/target/release/deps/librust_binding-0e2d7c5bf12a94fd.rlib: rust_binding/src/lib.rs
-
-/home/hbai/projects/go/src/github.com/eclipse-symphony/symphony/api/pkg/apis/v1alpha1/providers/target/rust/target/release/deps/rust_binding-0e2d7c5bf12a94fd.d: rust_binding/src/lib.rs
-
-rust_binding/src/lib.rs:
