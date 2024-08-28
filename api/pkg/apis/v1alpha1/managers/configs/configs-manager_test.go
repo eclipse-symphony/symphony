4935d4f4502d2ba64695fcb1a69741cc2b1d24fd
Jesse <iwangjintian@gmail.com>
2024-08-28 10:58:26 +0800

    using new tracing for config and secret managers and providers (#413)
    * using new tracing for config and secret managers and providers
    
    * fix wrong log method

--- api/pkg/apis/v1alpha1/managers/configs/configs-manager_test.go
+++ api/pkg/apis/v1alpha1/managers/configs/configs-manager_test.go
@@ -7,6 +7,7 @@
 package configs
 
 import (
+	"context"
 	"testing"
 
 	"github.com/eclipse-symphony/symphony/coa/pkg/apis/v1alpha2/managers"
@@ -15,6 +16,8 @@ import (
 	memory "github.com/eclipse-symphony/symphony/coa/pkg/apis/v1alpha2/providers/config/memoryconfig"
 	"github.com/stretchr/testify/assert"
 )
+
+var ctx = context.Background()
 
 func TestInit(t *testing.T) {
 	configProvider := memory.MemoryConfigProvider{}
@@ -215,8 +218,8 @@ func TestOverlayWithMultipleProviders(t *testing.T) {
 		Precedence: []string{"memory2", "memory1"},
 	}
 	assert.Nil(t, err)
-	provider1.Set("obj", "field", "obj::field")
-	provider2.Set("obj-overlay", "field", "overlay::field")
+	provider1.Set(ctx, "obj", "field", "obj::field")
+	provider2.Set(ctx, "obj-overlay", "field", "overlay::field")
 	val, err := manager.Get("obj", "field", []string{"obj-overlay"}, nil)
 	assert.Nil(t, err)
 	assert.Equal(t, "overlay::field", val)
@@ -248,7 +251,7 @@ func TestOverlayMissWithMultipleProviders(t *testing.T) {
 		Precedence: []string{"memory2", "memory1"},
 	}
 	assert.Nil(t, err)
-	provider1.Set("obj", "field", "obj::field")
+	provider1.Set(ctx, "obj", "field", "obj::field")
 	val, err := manager.Get("obj", "field", []string{"obj-overlay"}, nil)
 	assert.Nil(t, err)
 	assert.Equal(t, "obj::field", val)
@@ -280,8 +283,8 @@ func TestOverlayWithMultipleProvidersReversedPrecedence(t *testing.T) {
 		Precedence: []string{"memory1", "memory2"},
 	}
 	assert.Nil(t, err)
-	provider1.Set("obj", "field", "obj::field")
-	provider2.Set("obj-overlay", "field", "overlay::field")
+	provider1.Set(ctx, "obj", "field", "obj::field")
+	provider2.Set(ctx, "obj-overlay", "field", "overlay::field")
 	val, err := manager.Get("obj", "field", []string{"obj-overlay"}, nil)
 	assert.Nil(t, err)
 	assert.Equal(t, "obj::field", val)
@@ -314,8 +317,8 @@ func TestMultipleProvidersSameKey(t *testing.T) {
 		Precedence: []string{"memory2", "memory1"},
 	}
 	assert.Nil(t, err)
-	provider1.Set("obj", "field", "obj::field1")
-	provider2.Set("obj", "field", "obj::field2")
+	provider1.Set(ctx, "obj", "field", "obj::field1")
+	provider2.Set(ctx, "obj", "field", "obj::field2")
 	val, err := manager.Get("obj", "field", nil, nil)
 	assert.Nil(t, err)
 	assert.Equal(t, "obj::field2", val)
