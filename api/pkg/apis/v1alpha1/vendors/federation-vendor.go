be1a653c65c2424a79af8e92d8bb2af43ae7494f
Jiaxin Yan <jiaxinyan@microsoft.com>
2025-01-04 18:58:28 +0800

    test findAgent from Deployment State

--- api/pkg/apis/v1alpha1/vendors/federation-vendor.go
+++ api/pkg/apis/v1alpha1/vendors/federation-vendor.go
@@ -312,17 +312,18 @@ func (f *FederationVendor) Init(config vendors.VendorConfig, factories []manager
 	return f.SitesManager.UpsertState(context.Background(), f.Context.SiteInfo.SiteId, site)
 }
 func FindAgentFromDeploymentState(stepComponents []model.ComponentStep) bool {
-	return true
-	// log.Info("compare between state and target name %+v", stepComponents)
-	// for _, component := range stepComponents {
-	// 	log.Info("compare between state and target name %+v, %s", component, component.Component.Name)
-	// 	if component.Component.Type == "remote-agent" {
-	// 		log.Info("It is remote call ")
-	// 		return true
-	// 	}
+	log.Info("compare between state and target name %+v", stepComponents)
+	for _, component := range stepComponents {
+		log.Info("compare between state and target name %+v, %s", component, component.Component.Name)
+		if component.Component.Type == "remote-agent" {
+			log.Info("It is remote call ")
+			return true
+		} else {
+			log.Info(" it is not remote call %s", component.Component.Type)
+		}
 
-	// }
-	// return false
+	}
+	return false
 }
 func (f *FederationVendor) publishStepResult(ctx context.Context, target string, planId string, stepId int, Error error, components map[string]model.ComponentResultSpec) error {
 	return f.Vendor.Context.Publish("step-result", v1alpha2.Event{
