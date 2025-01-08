9a06df6f443c8ad79a778f3a995aef4661decc61
Jiaxin Yan <jiaxinyan@microsoft.com>
2025-01-08 10:21:43 +0800

    fix target result type bug

--- api/pkg/apis/v1alpha1/vendors/message_types.go
+++ api/pkg/apis/v1alpha1/vendors/message_types.go
@@ -76,7 +76,7 @@ type PlanState struct {
 	CurrentState         model.DeploymentState                    `json:"currentState"`
 	PreviousDesiredState *solution.SolutionManagerDeploymentState `json:"previous`
 	Status               string                                   `json:"status"`
-	TargetResult         map[string]int
+	TargetResult         map[string]model.ComponentSpec
 	Namespace            string `json:"namespace"`
 	Remove               bool
 	StepStates           []StepState
