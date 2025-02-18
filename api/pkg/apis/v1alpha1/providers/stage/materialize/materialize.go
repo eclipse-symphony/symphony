532059442c01ca9ac4fb35f5491b62286dff800b
Xingdong <xdlisjtu@gmail.com>
2025-02-18 17:38:53 +0800

    fixup

--- api/pkg/apis/v1alpha1/providers/stage/materialize/materialize.go
+++ api/pkg/apis/v1alpha1/providers/stage/materialize/materialize.go
@@ -628,6 +628,7 @@ func (i *MaterializeStageProvider) Process(ctx context.Context, mgrContext conte
 		}
 		mLog.InfofCtx(ctx, "  P (Materialize Processor): successfully waited for deployment to finish.")
 	}
+	outputs["failedDeploymentCount"] = len(outputs["failedDeployment"].([]api_utils.FailedDeployment))
 	return outputs, false, nil
 }
 
