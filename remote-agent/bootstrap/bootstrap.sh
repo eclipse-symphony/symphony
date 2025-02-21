0d2b3d142c7027b0a94398da242b9de9ebe7afc3
Jiaxin Yan <jiaxinyan@microsoft.com>
2025-02-21 17:01:14 +0800

    refine bootstrap shell

--- remote-agent/bootstrap/bootstrap.sh
+++ remote-agent/bootstrap/bootstrap.sh
@@ -7,13 +7,25 @@
 
 # Function to print usage
 usage() {
-    echo "Usage: $0 <endpoint> <cert_path> <key_path> <target_name> <namespace> <topology>"
+    echo -e "\e[31mUsage: $0 <endpoint> <cert_path> <key_path> <target_name> <namespace> <topology> <user> <group>\e[0m"
     exit 1
 }
 
+# Check if jq is installed
+if ! command -v jq &> /dev/null; then
+    echo -e "\e[33mjq is not installed. Installing jq...\e[0m"
+    sudo apt-get update && sudo apt-get install -y jq
+    if [ $? -ne 0 ]; then
+        echo -e "\e[31mError: Failed to install jq. Exiting...\e[0m"
+        exit 1
+    else
+        echo -e "\e[32mjq installed successfully.\e[0m"
+    fi
+fi
+
 # Check if the correct number of parameters are provided
 if [ "$#" -ne 8 ]; then
-    echo "Error: Invalid number of parameters."
+    echo -e "\e[31mError: Invalid number of parameters.\e[0m"
     usage
 fi
 
@@ -26,55 +38,57 @@ namespace=$5
 topology=$6
 user=$7
 group=$8
+
 # Validate the endpoint (basic URL validation)
 if ! [[ $endpoint =~ ^https?:// ]]; then
-    echo "Error: Invalid endpoint. Must be a valid URL starting with http:// or https://"
+    echo -e "\e[31mError: Invalid endpoint. Must be a valid URL starting with http:// or https://\e[0m"
     usage
 fi
 
 # Validate the certificate path (check if the file exists)
 if [ ! -f "$cert_path" ]; then
-    echo "Error: Certificate file not found at path: $cert_path"
+    echo -e "\e[31mError: Certificate file not found at path: $cert_path\e[0m"
     usage
 fi
 
-# Validate the certificate path (check if the file exists)
+# Validate the key path (check if the file exists)
 if [ ! -f "$key_path" ]; then
-    echo "Error: key file not found at path: $key_path"
+    echo -e "\e[31mError: Key file not found at path: $key_path\e[0m"
     usage
 fi
 
 # Validate the target name (non-empty string)
 if [ -z "$target_name" ]; then
-    echo "Error: Target name must be a non-empty string."
+    echo -e "\e[31mError: Target name must be a non-empty string.\e[0m"
     usage
 fi
 
 # Validate the namespace (default if not provided)
 if [ -z "$namespace" ]; then
-    echo "Error: namespace must be a non-empty string."
-    $namespace = "default"
+    echo -e "\e[31mError: Namespace must be a non-empty string.\e[0m"
+    namespace="default"
 fi
 
 # Validate the topology file (non-empty string)
 if [ -z "$topology" ]; then
-    echo "Error: Topology file must be a non-empty string."
+    echo -e "\e[31mError: Topology file must be a non-empty string.\e[0m"
     usage
 fi
 
 # Validate the user (non-empty string)
 if [ -z "$user" ]; then
-    echo "Error: User must be a non-empty string."
+    echo -e "\e[31mError: User must be a non-empty string.\e[0m"
     usage
-fi  
+fi
 
 # Validate the group (non-empty string)
 if [ -z "$group" ]; then
-    echo "Error: Group must be a non-empty string."
+    echo -e "\e[31mError: Group must be a non-empty string.\e[0m"
     usage
 fi
 
 # Create the JSON configuration
+echo -e "\e[32mCreating JSON configuration...\e[0m"
 config_json=$(cat <<EOF
 {
     "requestEndpoint": "$endpoint/solution/tasks",
@@ -87,23 +101,31 @@ EOF
 # Save the JSON configuration to a file
 config_file="config.json"
 echo "$config_json" > "$config_file"
+echo -e "\e[32mJSON configuration saved to $config_file\e[0m"
 
-
-# cert_path, key_path, topology_path, config to abosolute path
+# Convert cert_path, key_path, topology_path, config to absolute paths
 cert_path=$(realpath $cert_path)
 key_path=$(realpath $key_path)
 topology=$(realpath $topology)
 config=$(realpath $config_file)
 
-
-# call the endpoint with targetName and cert
+# Call the endpoint with targetName and cert
 bootstarpEndpoint="$endpoint/targets/bootstrap/$target_name?namespace=$namespace&osPlatform=linux"
-# read the topology file and POST as the body
+echo -e "\e[32mCalling bootstrap endpoint: $bootstarpEndpoint\e[0m"
+# Read the topology file and POST as the body
 TOPOLOGY_DATA=$(cat "$topology")
 
 result=$(curl --cert "$cert_path" --key "$key_path" -X POST "$bootstarpEndpoint" \
         -H "Content-Type: application/json" \
         -d "$TOPOLOGY_DATA")
+
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: Failed to call bootstrap endpoint. Please check the endpoint and try again.\e[0m"
+    exit 1
+else
+    echo -e "\e[32mBootstrap endpoint response received\e[0m"
+fi
+
 # Parse the JSON response and extract the fields
 public=$(echo $result | jq -r '.public')
 # Extract the header and footer
@@ -129,27 +151,53 @@ corrected_private_content="$header\n$base64_content\n$footer"
 
 # Save the public certificate to public.pem
 echo -e "$corrected_public_content" > public.pem
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: Failed to save public certificate to public.pem. Exiting...\e[0m"
+    exit 1
+else
+    echo -e "\e[32mPublic certificate saved to public.pem\e[0m"
+fi
 
 # Save the private key to private.pem
 echo -e "$corrected_private_content" > private.pem
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: Failed to save private key to private.pem. Exiting...\e[0m"
+    exit 1
+else
+    echo -e "\e[32mPrivate key saved to private.pem\e[0m"
+fi
 
 # Download the remote-agent binary
+echo -e "\e[32mDownloading remote-agent binary...\e[0m"
 curl --cert $cert_path --key $key_path -X GET "$endpoint/files/remote-agent" -o remote-agent
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: Failed to download remote-agent binary. Exiting...\e[0m"
+    exit 1
+else
+    echo -e "\e[32mRemote-agent binary downloaded\e[0m"
+fi
 
 # Make the remote-agent binary executable
 chmod +x remote-agent
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: Failed to make remote-agent binary executable. Exiting...\e[0m"
+    exit 1
+else
+    echo -e "\e[32mRemote-agent binary made executable\e[0m"
+fi
 
-echo "Files created successfully:"
-echo "public.pem"
-echo "private.pem"
-echo "remote-agent"
+echo -e "\e[32mFiles created successfully:\e[0m"
+echo -e "\e[32mpublic.pem\e[0m"
+echo -e "\e[32mprivate.pem\e[0m"
+echo -e "\e[32mremote-agent\e[0m"
 
-# public.pem, private.pem, remote-agent to abosolute path
+# Convert public.pem, private.pem, remote-agent to absolute paths
 public_path=$(realpath "./public.pem")
 private_path=$(realpath "./private.pem")
 agent_path=$(realpath "./remote-agent")
 
 # Create the remote-agent.service file
+echo -e "\e[32mCreating remote-agent.service file...\e[0m"
 sudo bash -c "cat <<EOF > /etc/systemd/system/remote-agent.service
 [Unit]
 Description=Remote Agent Service
@@ -164,15 +212,39 @@ Group=$group
 [Install]
 WantedBy=multi-user.target
 EOF"
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: Failed to create remote-agent.service file. Exiting...\e[0m"
+    exit 1
+else
+    echo -e "\e[32mremote-agent.service file created\e[0m"
+fi
 
 # Reload systemd to recognize the new service
 sudo systemctl daemon-reload
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: Failed to reload systemd daemon. Exiting...\e[0m"
+    exit 1
+else
+    echo -e "\e[32mSystemd daemon reloaded\e[0m"
+fi
 
 # Enable the service to start on boot
 sudo systemctl enable remote-agent.service
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: Failed to enable remote-agent.service. Exiting...\e[0m"
+    exit 1
+else
+    echo -e "\e[32mremote-agent.service enabled to start on boot\e[0m"
+fi
 
 # Start the service
 sudo systemctl start remote-agent.service
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: Failed to start remote-agent.service. Exiting...\e[0m"
+    exit 1
+else
+    echo -e "\e[32mremote-agent.service started\e[0m"
+fi
 
 # Check the status of the service
 sudo systemctl status remote-agent.service
