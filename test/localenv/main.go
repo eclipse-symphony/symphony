9943e2fc231cbbee609889e3d814e542940d559b
Jesse Huang <iwangjintian@gmail.com>
2025-03-01 11:37:25 +0800

    CA change; also update the bootstrap sample certs

--- test/localenv/main.go
+++ test/localenv/main.go
@@ -12,7 +12,7 @@ import (
 
 type RemoteAgentProvider struct{}
 
-func (i *RemoteAgentProvider) getCertificateExpirationOrThumbPrint(certPath string, kind string) (string, error) {
+func (i *RemoteAgentProvider) getCertificateExpirationOrThumbPrintOrSubject(certPath string, kind string) (string, error) {
 	certPEM, err := os.ReadFile(certPath)
 	if err != nil {
 		return "", err
@@ -27,28 +27,40 @@ func (i *RemoteAgentProvider) getCertificateExpirationOrThumbPrint(certPath stri
 	if err != nil {
 		return "", err
 	}
-	if kind == "thumbprint" {
+
+	switch kind {
+	case "thumbprint":
 		thumbprint := sha1.Sum(cert.Raw)
 		return hex.EncodeToString(thumbprint[:]), nil
-	} else {
+	case "expiration":
 		return cert.NotAfter.Format(time.RFC3339), nil
+	case "subject":
+		return cert.Subject.String(), nil
+	default:
+		return "", fmt.Errorf("invalid kind: %s", kind)
 	}
 }
 
 func main() {
 	provider := &RemoteAgentProvider{}
-	thumbprint, err := provider.getCertificateExpirationOrThumbPrint("test-cert.pem", "thumbprint")
+	thumbprint, err := provider.getCertificateExpirationOrThumbPrintOrSubject("test-cert.pem", "thumbprint")
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
 		fmt.Println("Thumbprint:", thumbprint)
 	}
 
-	expiredAt, err := provider.getCertificateExpirationOrThumbPrint("test-cert.pem", "expiration")
+	expiredAt, err := provider.getCertificateExpirationOrThumbPrintOrSubject("test-cert.pem", "expiration")
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
 		fmt.Println("ExpiredAt:", expiredAt)
 	}
 
+	subject, err := provider.getCertificateExpirationOrThumbPrintOrSubject("test-cert.pem", "subject")
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Subject:", subject)
+	}
 }
