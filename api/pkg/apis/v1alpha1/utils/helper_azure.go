39eddd64af335bda347d3a8e0dd885a5fe74629c
Xingdong Li <xingdli@microsoft.com>
2025-03-04 15:10:34 +0800

    add azure build flag

--- api/pkg/apis/v1alpha1/utils/helper_azure.go
+++ api/pkg/apis/v1alpha1/utils/helper_azure.go
@@ -6,7 +6,7 @@
  * SPDX-License-Identifier: MIT
  */
 
-package helper
+package utils
 
 import (
 	"context"
@@ -16,10 +16,30 @@ import (
 
 	"github.com/eclipse-symphony/symphony/api/constants"
 	"github.com/eclipse-symphony/symphony/api/pkg/apis/v1alpha1/model"
-	api_utils "github.com/eclipse-symphony/symphony/api/pkg/apis/v1alpha1/utils"
 	"github.com/eclipse-symphony/symphony/coa/pkg/apis/v1alpha2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+const (
+	AzureSolutionVersionIdPattern = "^/subscriptions/([0-9a-fA-F-]+)/resourcegroups/([^/]+)/providers/([^/]+)/targets/([^/]+)/solutions/([^/]+)/versions/([^/]+)$"
+	AzureTargetIdPattern          = "^/subscriptions/([0-9a-fA-F-]+)/resourcegroups/([^/]+)/providers/([^/]+)/targets/([^/]+)$"
+)
+
+func ConvertAzureSolutionVersionReferenceToObjectName(name string) (string, bool) {
+	r := regexp.MustCompile(AzureSolutionVersionIdPattern)
+	if !r.MatchString(name) {
+		return "", false
+	}
+	return r.ReplaceAllString(name, fmt.Sprintf("$4%s$5%s$6", constants.ResourceSeperator, constants.ResourceSeperator)), true
+}
+
+func ConvertAzureTargetReferenceToObjectName(name string) (string, bool) {
+	r := regexp.MustCompile(AzureTargetIdPattern)
+	if !r.MatchString(name) {
+		return "", false
+	}
+	return r.ReplaceAllString(name, "$4"), true
+}
 
 func GetInstanceTargetName(name string) string {
 	parts := strings.Split(name, "/")
@@ -39,7 +59,7 @@ func GetInstanceRootResource(name string) string {
 	return parts[len(parts)-3]
 }
 
-func GetInstanceOwnerReferences(apiClient api_utils.ApiClient, ctx context.Context, objectName string, objectNamespace string, instanceState model.InstanceState, user string, pwd string) ([]metav1.OwnerReference, error) {
+func GetInstanceOwnerReferences(apiClient ApiClient, ctx context.Context, objectName string, objectNamespace string, instanceState model.InstanceState, user string, pwd string) ([]metav1.OwnerReference, error) {
 	parts := strings.Split(instanceState.Spec.Solution, constants.ReferenceSeparator)
 	if len(parts) != 2 {
 		return nil, v1alpha2.NewCOAError(nil, fmt.Sprintf("Invalid solution name: instance - %s", objectName), v1alpha2.BadRequest)
@@ -58,7 +78,7 @@ func GetInstanceOwnerReferences(apiClient api_utils.ApiClient, ctx context.Conte
 	}, nil
 }
 
-func GetSolutionContainerOwnerReferences(apiClient api_utils.ApiClient, ctx context.Context, objectName string, objectNamespace string, user string, pwd string) ([]metav1.OwnerReference, error) {
+func GetSolutionContainerOwnerReferences(apiClient ApiClient, ctx context.Context, objectName string, objectNamespace string, user string, pwd string) ([]metav1.OwnerReference, error) {
 	target, err := apiClient.GetTarget(ctx, objectName, objectNamespace, user, pwd)
 	if err != nil {
 		return nil, err
@@ -85,4 +105,15 @@ func isPrivateResourceProvider(resourceId string) bool {
 	pattern := `^/subscriptions/([0-9a-fA-F-]+)/resourcegroups/([^/]+)/providers/private.edge/.*`
 	re := regexp.MustCompile(pattern)
 	return re.MatchString(strings.ToLower(resourceId))
+}
+
+func ConvertReferenceToObjectNameHelper(name string) string {
+	// deal with Azure pattern
+	if n, ok := ConvertAzureSolutionVersionReferenceToObjectName(name); ok {
+		return n
+	}
+	if n, ok := ConvertAzureTargetReferenceToObjectName(name); ok {
+		return n
+	}
+	return name
 }
