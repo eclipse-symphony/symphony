1ae854012b7d11ebf60a92dcd118a1cacb90d784
linyguo <lingyunguo@microsoft.com>
2025-04-22 16:01:34 +0800

    update property name

--- api/pkg/apis/v1alpha1/managers/solution/summary-cleanup-manager.go
+++ api/pkg/apis/v1alpha1/managers/solution/summary-cleanup-manager.go
@@ -36,7 +36,7 @@ func (s *SummaryCleanupManager) Init(ctx *vendorCtx.VendorContext, config manage
 	}
 
 	// Set activation cleanup interval after they are done. If not set, use default 180 days.
-	if val, ok := config.Properties["SummaryRetentionDuration"]; ok {
+	if val, ok := config.Properties["RetentionDuration"]; ok {
 		s.SummaryRetentionDuration, err = time.ParseDuration(val)
 		if err != nil {
 			return v1alpha2.NewCOAError(nil, "SummaryRetentionDuration cannot be parsed, please enter a valid duration", v1alpha2.BadConfig)
