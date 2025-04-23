7f716f2c562f3581ca7b248acfa962a843eecdba
Jiaxin Yan <jiaxinyan@microsoft.com>
2025-04-23 18:19:18 +0800

    fix issue

--- api/pkg/apis/v1alpha1/managers/solution/summary-cleanup-manager.go
+++ api/pkg/apis/v1alpha1/managers/solution/summary-cleanup-manager.go
@@ -20,7 +20,8 @@ import (
 
 const (
 	// DefaultSummaryRetentionDuration is the default time to cleanup deprecated summaries
-	DefaultSummaryRetentionDuration = 180 * time.Hour * 24
+	// DefaultSummaryRetentionDuration = 180 * time.Hour * 24
+	DefaultSummaryRetentionDuration = 60 * time.Second * 5
 )
 
 type SummaryCleanupManager struct {
@@ -35,7 +36,7 @@ func (s *SummaryCleanupManager) Init(ctx *vendorCtx.VendorContext, config manage
 	}
 
 	// Set activation cleanup interval after they are done. If not set, use default 180 days.
-	if val, ok := config.Properties["RetentionDuration"]; ok {
+	if val, ok := config.Properties["SummaryRetentionDuration"]; ok {
 		s.SummaryRetentionDuration, err = time.ParseDuration(val)
 		if err != nil {
 			return v1alpha2.NewCOAError(nil, "SummaryRetentionDuration cannot be parsed, please enter a valid duration", v1alpha2.BadConfig)
