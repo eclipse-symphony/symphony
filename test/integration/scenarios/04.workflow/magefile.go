86411e6fb346dee2633e598ccf5abc0376274eca
Haishi Bai <hbai@microsoft.com>
2025-11-04 11:33:45 -0800

    update integration 04 scenario

--- test/integration/scenarios/04.workflow/magefile.go
+++ test/integration/scenarios/04.workflow/magefile.go
@@ -67,16 +67,15 @@ var (
 // Entry point for running the tests
 func Test(labeling bool) error {
 	fmt.Println("Running ", TEST_NAME, " with labeling:", labeling)
-
+	// defer Cleanup()
+	var err error
 	if labeling {
-		err := modifyYAML("localtest", "management.azure.com/azureName")
-		if err != nil {
-			return err
-		}
+		err = testhelpers.SetClusterWithSetting("\"--set api.labelKey=localhost --set api.labelValue=localhost --set api.annotationKey=management.azure.com/azureName\"")
 		os.Setenv("labelingEnabled", "true")
+	} else {
+		err = testhelpers.SetupCluster()
 	}
-	defer Cleanup()
-	err := testhelpers.SetupCluster()
+
 	if err != nil {
 		return err
 	}
@@ -242,9 +241,6 @@ func modifyYAML(v string, annotationKey string) error {
 	if err != nil {
 		return err
 	}
-
-	fmt.Printf("Saved YAML content: %s\n", string(data))
-	fmt.Printf("Saved YAML file to: %s\n", ghcrValueFilePath)
 
 	// Write the modified YAML data back to the file
 	err = os.WriteFile(ghcrValueFilePath, data, 0644)
