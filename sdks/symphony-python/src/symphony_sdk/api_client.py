c71dff1c51c6e96f5645afae2a289a5bdc9d881e
Ibrahim Sel <ibrahim.sel@eteration.com>
2026-01-14 14:56:12 +0300

    match license headers to rest of the repo

--- sdks/symphony-python/src/symphony_sdk/api_client.py
+++ sdks/symphony-python/src/symphony_sdk/api_client.py
@@ -1,23 +1,8 @@
-#
-# Copyright (c) 2025 Composiv.ai
-#
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License v2.0
-# and Eclipse Distribution License v1.0 which accompany this distribution.
-#
-# Licensed under the Eclipse Public License v2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# The Eclipse Public License is available at
-#    http://www.eclipse.org/legal/epl-v20.html
-# and the Eclipse Distribution License is available at
-#   http://www.eclipse.org/org/documents/edl-v10.php.
-#
-# Contributors:
-#    Composiv.ai - initial API and implementation
-#
-
 """Symphony REST API Client.
+
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+SPDX-License-Identifier: MIT
 
 This module provides a client for interacting with the Symphony REST API,
 including authentication, target registration/unregistration, and other
